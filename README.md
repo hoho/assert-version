4379037529f9fe9917d7cbc91e7edd4099ed7c83
Marat Abdullin <dakota@brokenpipe.ru>
2014-08-10 02:10:27 +0400

    First version.

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-assert-version
-==============
+# assert-version
 
 Check if version mentions match the one from package.json
+
+
+## Install
+
+`npm install -g assert-version`
+
+
+## How matches work
+
+Version could be extracted with regular expression or, if you use
+`assert-version` as Node.js module, with user-defined function.
+
+In case of regular expression, version should present in first capture.
+
+There are two default regular expressions. If your file has `.json` extension,
+`/"version":\s*"([^"]+)/` expression is used. Otherwise,
+`/\/\*\![\s\S]+(v?\d+\.\d+\.\d+)/` expression is used. I understand that these
+expressions are not great, they just seem to work for my cases. Feel free to
+send improvements.
+
+
+## Use as Node.js module
+
+```js
+assertVersion = require('assert-version');
+
+var error = assertVersion({
+    'bower.json': '', // First default regular expression will be used.
+    'mylib.js': '', // Second default regular expression will be used.
+    'other.js': /version=([^,]+)/,
+    'other2.js': function(contents) {
+        // `contents` is a file contents, function should return version as
+        // string.
+        return contents.substring(100500, 100510);
+    },
+    'other3.js': [matcher1, matcher2] // If you need to check more than one place in file.
+});
+
+Optionally, you can pass path to `package.json` with reference version as
+second argument of `assertVersion()`. It is `package.json` from current
+working directory by default.
+
+// `error` will have error message in case of error, `undefined` otherwise.
+```
+
+
+## Use as command line tool
+
+Same to previous example (except for function extractor):
+
+```
+assert-version -f bower.json -f mylib.js -f other.js='version=([^,]+)' -f other3.js=matcher1 -f other3.js=matcher2
+```
--- assert-version.js
+++ assert-version.js
@@ -1,5 +1,64 @@
-require('colors');
+var semver = require('semver');
+var fs = require('fs');
+var path = require('path');
+var format = require('util').format;
 
-module.exports = function assertVersion(files, jsonPath, update) {
-    
+module.exports = function assertVersion(files, jsonPath) {
+    var file;
+    var version;
+    var defaultRegExp = /"version":\s*"([^"]+)/; // For `*.json` files.
+    var defaultRegExp2 = /\/\*\![\s\S]+(v?\d+\.\d+\.\d+)/; // For other files.
+
+    jsonPath = jsonPath || 'package.json';
+
+    try {
+        version = JSON.parse(fs.readFileSync(jsonPath, {encoding: 'utf8'})).version;
+    } catch(e) {}
+
+    if (!((version = semver.valid(version)))) {
+        return format('Invalid version in `%s`', jsonPath);
+    }
+
+    for (file in files) {
+        var checkers = files[file],
+            checker,
+            contents,
+            ver;
+
+        try {
+            contents = fs.readFileSync(file, {encoding: 'utf8'});
+        } catch(e) {
+            return format('Failed to read `%s`', file);
+        }
+
+        if (!(checkers instanceof Array)) {
+            checkers = [checkers];
+        }
+
+        for (var i = 0; i < checkers.length; i++) {
+            checker = checkers[i] || (path.extname(file) === '.json' ? defaultRegExp : defaultRegExp2);
+
+            if (typeof checker === 'function') {
+                ver = checker(contents);
+
+                if (typeof ver !== 'string') {
+                    return format('Version extractor result for `%s` is not a string', file);
+                }
+            } else {
+                if (!(checker instanceof RegExp)) {
+                    checker = new RegExp(checker);
+                }
+
+                ver = contents.match(checker);
+
+                if (!ver || !((ver = ver[1]))) {
+                    return format('Failed to find version in `%s` with %s regular expression', file, checker);
+                }
+            }
+
+            if (ver !== version) {
+                return format('Version mismatch (`%s` !== `%s` in `%s`)', ver, version, file);
+            }
+        }
+    }
 };
